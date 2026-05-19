dbb5e5fc25130dcf9539cd22c83572e14dc6444a
grejfla <grejfla@gmail.com>
2026-05-19 19:32:35 -0300

    Criado Programa Salário Mínimo| Flávio.

---
+++ principal.py
@@ -0,0 +1,4 @@
+salario_minimo = float (input ("Digite o valor do salário mínimo: "))
+salario_usuario = float (input ("Digite o valor de seu sálario: "))
+quantidade = salario_usuario / salario_minimo
+print (f"quantidade de salários = {quantidade:.2f}.")
